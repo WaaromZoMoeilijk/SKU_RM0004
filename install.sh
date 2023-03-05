10a1ccd2aae2263d005c52b89c3e2f426ae9c709
Xndr <underdeveloment@gmail.com>
2023-03-04 20:47:29 -0500

    Whoops! Forgot to update service name.

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ sudo mkdir -p /opt/UCTRONICS/SKU_RM0004
 sudo cp ./display /opt/UCTRONICS/SKU_RM0004
 sudo cp ./SKU_RM0004.service /etc/systemd/system
 sudo systemctl daemon-reload
-sudo systemctl enable --now RM0004
+sudo systemctl enable --now SKU_RM0004
